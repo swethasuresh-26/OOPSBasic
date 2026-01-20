61a00e072fc0ee672e0627ab17d035f087292512
swethasuresh-26 <swethasuresh.tech26@gmail.com>
2026-01-20 13:31:07 +0530

    Concurrency Topic

--- src/Concurrency/Main.java
+++ src/Concurrency/Main.java
@@ -1,10 +1,37 @@
 package Concurrency;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     static void main(String[] args) {
-        HelloWorldPrinter hwp=new HelloWorldPrinter();
+   /*     HelloWorldPrinter hwp=new HelloWorldPrinter();
         Thread t=new Thread(hwp);
         t.start();
         System.out.println("Hello World : "+Thread.currentThread().getName());
+    */
+
+        ExecutorService executorService= Executors.newFixedThreadPool(25);
+        Instant start=Instant.now();
+
+        for(int i=1;i<=100;i++)
+        {
+            if(i==5 || i==45 ||i==90)
+                System.out.println("Debug");
+            NumberPrinter n=new NumberPrinter(i);
+          /* Thread t=new Thread(n);
+            t.start();
+            * */
+            executorService.submit(n);
+            }
+       // Instant end=Instant.now();
+        //Duration duration=Duration.between(start,end);
+        //System.out.println(duration.toMillis());
     }
 }
+//Thread start from zero in normal thread
+//In executor Service it start from 1
+//WorkQueue==Number of tasks we want to execute
+//Workers = Number of Threads in Thread Pool
--- src/Concurrency/NumberPrinter.java
+++ src/Concurrency/NumberPrinter.java
@@ -1,8 +1,13 @@
 package Concurrency;
 
 public class NumberPrinter implements Runnable{
+  private int number;
+  NumberPrinter(int number)
+  {
+      this.number=number;
+  }
     @Override
     public void run() {
-        //System.out.println(+Thread.currentThread().getName());
+        System.out.println(number+" "+Thread.currentThread().getName());
     }
 }
