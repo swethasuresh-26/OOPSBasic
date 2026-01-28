eafd274dcd803c82fb77e06875b83c28042b4ed1
swethasuresh-26 <swethasuresh.tech26@gmail.com>
2026-01-28 11:00:00 +0530

    Added Generic Folder

---
+++ src/Generic/Pair.java
@@ -0,0 +1,16 @@
+package Generic;
+
+public class Pair <A,B>{
+    public A first;
+    public B second;
+
+    public Pair(A first,B second)
+    {
+        this.first=first;
+        this.second=second;
+    }
+    public void print(A a,B b)
+    {
+        System.out.println(a+" -> "+b);
+    }
+}
